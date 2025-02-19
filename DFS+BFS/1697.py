9735751c9e1d681fee57d01e26185393ef2a86e1
Lee Ha-eun <lheun99@gmail.com>
2025-02-19 22:08:00 +0900

    깃 고치기

--- DFS+BFS/1697.py
+++ DFS+BFS/1697.py
@@ -1,25 +1,34 @@
+from collections import deque
+
 n, k = map(int, input().split())
 
 dx = [-1, +1, 2]
 
 time = 0
 
-from collections import deque
+visited = [False] * 100001
+visited[n] =True
 q = deque([(n, time)])
 
-for z in range(5):
+while q:
     x, time = q.popleft()
 
-    if x < k:
-        nx = x
-        for i in range(3):
-            if dx[i] == 2:
-                nx = x * 2
-            else:
-                nx = x + dx[i]
-            
-            q.append((nx, time+1))
-    else:
+    #print(f'x = {x}, time = {time}')
+
+    if x == k:
+        print(time)
         break
 
-print(time)
+
+    for i in range(3):
+        if i == 2:
+            nx = x * 2
+        else:
+            nx = x + dx[i]
+
+        if 0<=nx<=100000 and not visited[nx]:
+            q.append((nx, time+1))
+            visited[nx] = True
+
+
+
--- DFS+BFS/7576.py
+++ DFS+BFS/7576.py
@@ -1,42 +1,40 @@
 from collections import deque
 
-m, n = map(int, input().split())
-arr = []*n
+m, n = map(int,input().split())
 
+arr = []
 for _ in range(n):
     arr.append(list(map(int, input().split())))
 
-dx = [0, 0, 1, -1]
-dy = [-1, 1, 0, 0]
-
-#시작점
 q = deque()
+
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
+
+
 for i in range(n):
     for j in range(m):
         if arr[i][j] == 1:
             q.append((i, j, 0))
 
-days = 0
+max_d = 0
 while q:
-    x, y, days = q.popleft()
-
-    #max_days = max(max_days, days)  # 최대 날짜 갱신
-
+    x, y, day = q.popleft()
+    max_d = max(max_d, day)
+    #print(x, y, day)
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
 
         if 0<=nx<n and 0<=ny<m:
             if arr[nx][ny] == 0:
-                q.append((nx, ny, days+1))
+                q.append((nx, ny, day+1))
                 arr[nx][ny] = 1
-days = 0
-for r in arr:
-    if 0 in r:
+
+for row in arr:
+    if 0 in row:
         print(-1)
-        break
+        exit(0)
+print(max_d)
 
-    max(max(r), days)
-
-else: 
-    print(days)
+##
